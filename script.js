3dba3b907ae8a06ee0c418351f88828e4688509a
Soumyajit Bag <soumyajitbag2001@gmail.com>
2022-08-23 02:35:12 +0530

    Add delete option and local storage

--- script.js
+++ script.js
@@ -1,13 +1,55 @@
-function add(){
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("data").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if(inputValue === ''){
-      alert("Empty!! Write something ")
+const input = document.getElementById("data");
+const list = document.getElementById("list");
+const btn = document.getElementById("btn");
+
+const tasks = localStorage.getItem("tasks")? 
+JSON.parse(localStorage.getItem("tasks"))
+    :[];
+
+showTasks();
+
+function showTasks(){
+    tasks.forEach((e,index)=>{
+    const li = document.createElement("li");
+    li.className = "task";
+
+    const p = document.createElement("p");
+    li.append(p);
+    p.innerText = (index+1)+". "+ e;
+
+    const span = document.createElement("span");
+    span.className = "del";
+    li.append(span);
+
+    // icon
+    const i = document.createElement("i");
+    i.className="bi bi-trash"; //del icon
+    span.appendChild(i);
+
+    span.addEventListener("click",()=>{
+        removeTasks();
+        tasks.splice(index,1);
+        localStorage.setItem("tasks",JSON.stringify(tasks));
+        showTasks();
+    })
+
+    list.appendChild(li);
+    })
+}
+const removeTasks=()=>{
+    tasks.forEach((e)=>{
+        const div = document.querySelector(".task");
+        div.remove();
+    })
+}
+btn.addEventListener("click",()=>{
+    if(input.value === ''){
+        alert("Empty!! Write something ");
     } else {
-      document.getElementById("list").appendChild(li);
+        removeTasks();
+        tasks.push(input.value);
+        localStorage.setItem("tasks",JSON.stringify(tasks));
+        showTasks();
+        input.value='';
     }
-    document.getElementById("data").value='';
-    // inputValue = '';
-  }
+})
